ba6939d3610db142cb21a77a88caa5b7470c5376
callsaul <itsallgood.man@yandex.ru>
2025-06-04 22:55:47 +0300

    feat: confirm message via extension

--- example/ios/AppNotiService/NotificationService.m
+++ example/ios/AppNotiService/NotificationService.m
@@ -25,6 +25,15 @@
     // Send SHOW interaction event
     [self sendInteractionEvent:1 withUserInfo:request.content.userInfo];
     
+    // Confirm the message if messageId is present
+    NSString *messageId = request.content.userInfo[@"messageId"];
+    if (messageId && [messageId isKindOfClass:[NSString class]]) {
+        NSLog(@"[Extension] Confirming message with ID: %@", messageId);
+        [self confirmMessage:messageId withUserInfo:request.content.userInfo];
+    } else {
+        NSLog(@"[Extension] No messageId found for confirmation");
+    }
+    
     // Modify the notification content here...
     self.bestAttemptContent.title = [NSString stringWithFormat:@"%@ [modified]", self.bestAttemptContent.title];
     
@@ -36,6 +45,83 @@
     // Called just before the extension will be terminated by the system.
     // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
     self.contentHandler(self.bestAttemptContent);
+}
+
+// MARK: - Message Confirmation
+
+- (void)confirmMessage:(NSString *)messageId withUserInfo:(NSDictionary *)userInfo {
+    NSLog(@"[Extension Confirm] Starting message confirmation for messageId: %@", messageId);
+    
+    // Get clientToken from shared UserDefaults
+    NSUserDefaults *sharedDefaults = [[NSUserDefaults alloc] initWithSuiteName:@"group.pushed.example"];
+    NSString *clientToken = [sharedDefaults stringForKey:@"clientToken"];
+    
+    // Fallback to file if UserDefaults fails
+    if (!clientToken || clientToken.length == 0) {
+        NSURL *sharedURL = [[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:@"group.pushed.example"];
+        if (sharedURL) {
+            NSURL *tokenFileURL = [sharedURL URLByAppendingPathComponent:@"clientToken.txt"];
+            NSError *error;
+            clientToken = [NSString stringWithContentsOfURL:tokenFileURL encoding:NSUTF8StringEncoding error:&error];
+            if (error) {
+                NSLog(@"[Extension Confirm] ERROR: Could not read clientToken from file: %@", error.localizedDescription);
+            }
+        }
+    }
+    
+    if (!clientToken || clientToken.length == 0) {
+        NSLog(@"[Extension Confirm] ERROR: clientToken is empty, cannot confirm message");
+        return;
+    }
+    
+    NSLog(@"[Extension Confirm] Using clientToken: %@...", [clientToken substringToIndex:MIN(10, clientToken.length)]);
+    
+    // Create Basic Auth: clientToken:messageId
+    NSString *credentials = [NSString stringWithFormat:@"%@:%@", clientToken, messageId];
+    NSData *credentialsData = [credentials dataUsingEncoding:NSUTF8StringEncoding];
+    NSString *basicAuth = [NSString stringWithFormat:@"Basic %@", [credentialsData base64EncodedStringWithOptions:0]];
+    
+    // Create URL and request
+    NSString *urlString = @"https://pub.pushed.ru/v1/confirm?transportKind=Apns";
+    NSURL *url = [NSURL URLWithString:urlString];
+    if (!url) {
+        NSLog(@"[Extension Confirm] ERROR: Invalid URL: %@", urlString);
+        return;
+    }
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"POST";
+    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request addValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    [request addValue:basicAuth forHTTPHeaderField:@"Authorization"];
+    
+    NSLog(@"[Extension Confirm] Sending confirmation request to: %@", urlString);
+    NSLog(@"[Extension Confirm] Authorization header created for messageId: %@", messageId);
+    
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error) {
+            NSLog(@"[Extension Confirm] Request error: %@", error.localizedDescription);
+            return;
+        }
+        
+        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+        if (!httpResponse) {
+            NSLog(@"[Extension Confirm] ERROR: No HTTPURLResponse");
+            return;
+        }
+        
+        NSInteger status = httpResponse.statusCode;
+        NSString *responseBody = data ? [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] : @"<no body>";
+        
+        if (status >= 200 && status < 300) {
+            NSLog(@"[Extension Confirm] SUCCESS - Status: %ld, Body: %@", (long)status, responseBody);
+        } else {
+            NSLog(@"[Extension Confirm] ERROR - Status: %ld, Body: %@", (long)status, responseBody);
+        }
+    }];
+    
+    [task resume];
+    NSLog(@"[Extension Confirm] Confirmation request sent for messageId: %@", messageId);
 }
 
 // MARK: - Interaction Events
